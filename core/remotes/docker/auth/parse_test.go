38375181fdfd6577f5b075fd621a1195c90efc4f
yechun <yechun@corp.netease.com>
2025-03-29 19:26:06 +0800

    test

--- core/remotes/docker/auth/parse_test.go
+++ core/remotes/docker/auth/parse_test.go
@@ -17,74 +17,9 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
-	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
-
-func TestParseAuthHeaderBearer(t *testing.T) {
-	headerTemplate := `Bearer realm="%s",service="%s",scope="%s"`
-
-	for _, tc := range []struct {
-		name    string
-		realm   string
-		service string
-		scope   string
-	}{
-		{
-			name:    "SingleScope",
-			realm:   "https://auth.docker.io/token",
-			service: "registry.docker.io",
-			scope:   "repository:foo/bar:pull,push",
-		},
-		{
-			name:    "MultipleScopes",
-			realm:   "https://auth.docker.io/token",
-			service: "registry.docker.io",
-			scope:   "repository:foo/bar:pull,push repository:foo/baz:pull repository:foo/foo:push",
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			expected := []Challenge{
-				{
-					Scheme: BearerAuth,
-					Parameters: map[string]string{
-						"realm":   tc.realm,
-						"service": tc.service,
-						"scope":   tc.scope,
-					},
-				},
-			}
-
-			hdr := http.Header{
-				http.CanonicalHeaderKey("WWW-Authenticate"): []string{fmt.Sprintf(
-					headerTemplate, tc.realm, tc.service, tc.scope,
-				)},
-			}
-			actual := ParseAuthHeader(hdr)
-			if !reflect.DeepEqual(expected, actual) {
-				t.Fatalf("expected %v, but got %v", expected, actual)
-			}
-		})
-	}
-}
-
-func TestParseAuthHeader(t *testing.T) {
-	v := `Bearer realm="https://auth.example.io/token",empty="",service="registry.example.io",scope="repository:library/hello-world:pull,push"`
-	h := http.Header{http.CanonicalHeaderKey("WWW-Authenticate"): []string{v}}
-	challenge := ParseAuthHeader(h)
-
-	actual, ok := challenge[0].Parameters["empty"]
-	assert.True(t, ok)
-	assert.Equal(t, "", actual)
-
-	actual, ok = challenge[0].Parameters["service"]
-	assert.True(t, ok)
-	assert.Equal(t, "registry.example.io", actual)
-}
 
 func FuzzParseAuthHeader(f *testing.F) {
 	f.Add(`Bearer realm="https://example.com/token",service="example.com",scope="repository:foo/bar:pull,push"`)
